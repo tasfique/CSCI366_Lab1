5102563bf0fdcacf6b3f396347902f797b84f4c9
JustTaz <j16020825@student.newinti.edu.my>
2020-08-27 21:21:31 +0800

    Version 2

--- app/src/main/java/com/csci366_2020/tasfique/calculator/MainActivity.java
+++ app/src/main/java/com/csci366_2020/tasfique/calculator/MainActivity.java
@@ -1,20 +1,22 @@
 package com.csci366_2020.tasfique.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0, btn_Add, btn_Sub, btn_Mul, btn_Div, btn_calc, btn_dec, btn_clear;
-    EditText mEditText;
-    String input = "";
+    TextView mTextView, mTextView2;
+    String process;
 
-    float Value1, Value2, Value3, Value4;
-    boolean mAddition, mSubtract, mMultiplication, mDivision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,201 +40,221 @@ public class MainActivity extends AppCompatActivity {
         btn_calc = (Button) findViewById(R.id.btn_calc);
         btn_dec = (Button) findViewById(R.id.btn_dec);
         btn_clear = (Button) findViewById(R.id.btn_clear);
-        mEditText = (EditText) findViewById(R.id.edText1);
+        mTextView = (TextView) findViewById(R.id.edText1);
+        mTextView2 = (TextView) findViewById(R.id.edText2);
 
         btn_0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //mEditText.setText(mEditText.getText() + "0");
-                input +=  mEditText.getText() + "0";
-                mEditText.setText(input);
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "0");
             }
         });
 
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //mEditText.setText(mEditText.getText() + "1");
-                input += input + mEditText.getText() + "1";
-                mEditText.setText(input);
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "1");
             }
         });
 
         btn_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //mEditText.setText(mEditText.getText() + "2");
-                input += mEditText.getText() + "2";
-                mEditText.setText(input);
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "2");
             }
         });
 
         btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //mEditText.setText(mEditText.getText() + "3");
-                input += mEditText.getText() + "3";
-                mEditText.setText(input);
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "3");
             }
         });
 
         btn_4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "4");
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "4");
             }
         });
 
         btn_5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "5");
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "5");
             }
         });
 
         btn_6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "6");
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "6");
             }
         });
 
         btn_7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "7");
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "7");
             }
         });
 
         btn_8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "8");
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "8");
             }
         });
 
         btn_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + "9");
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "9");
             }
         });
 
         btn_dec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText(mEditText.getText() + ".");
+                process = mTextView.getText().toString();
+                mTextView.setText(process + ".");
             }
         });
 
         btn_Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "+");
             }
         });
 
         btn_Sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "-");
             }
         });
 
         btn_Mul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                input+="*";
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "*");
             }
         });
 
         btn_Div.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                process = mTextView.getText().toString();
+                mTextView.setText(process + "/");
             }
         });
 
         btn_calc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(input.split("\\*").length==2){
-                    String numbers[]=input.split("\\*");
-                    try{
-                        double mul=Double.parseDouble(numbers[0])*Double.parseDouble(numbers[1]);
-                        input=mul+"";
-                    }
-                    catch (Exception e){
-                        //Display error
-                    }
-                }
-                else if(input.split("/").length==2){
-                    String numbers[]=input.split("/");
-                    try{
-                        double div=Double.parseDouble(numbers[0])/Double.parseDouble(numbers[1]);
-                        input=div+"";
-                    }
-                    catch (Exception e){
-                        //Display error
-                    }
-                }
-                else if(input.split("\\^").length==2){
-                    String numbers[]=input.split("\\^");
-                    try{
-                        double pow=Math.pow(Double.parseDouble(numbers[0]),Double.parseDouble(numbers[1]));
-                        input=pow+"";
-                    }
-                    catch (Exception e){
-                        //Display error
-                    }
-                }
-                else if(input.split("\\+").length==2){
-                    String numbers[]=input.split("\\+");
-                    try{
-                        double sum=Double.parseDouble(numbers[0])+Double.parseDouble(numbers[1]);
-                        input=sum+"";
-                    }
-                    catch (Exception e){
-                        //Display error
-                    }
-                }
-                else if(input.split("\\-").length>1){
-                    String numbers[]=input.split("\\-");
-                    if(numbers[0]=="" && numbers.length==2){
-                        numbers[0]=0+"";
-                    }
-                    try{
-                        double sub=0;
-                        if(numbers.length==2) {
-                            sub = Double.parseDouble(numbers[0]) - Double.parseDouble(numbers[1]);
-                        }
-                        else if(numbers.length==3){
-                            sub = -Double.parseDouble(numbers[1]) - Double.parseDouble(numbers[2]);
-                        }
-                        input=sub+"";
-                    }
-                    catch (Exception e){
-                        //Display error
-                    }
-                }
-                String n[]=input.split("\\.");
-                if(n.length>1){
-                    if(n[1].equals("0")){
-                        input=n[0];
-                    }
-                }
-                mEditText.setText(input);
+                process = mTextView.getText().toString();
+                process = process.replaceAll("x", "*");
+                //process = process.replaceAll("")
+                Context rhino =  Context.enter();
 
+                rhino.setOptimizationLevel(-1);
+
+                String finalResult = "";
+
+                try {
+                    Scriptable scriptable = rhino.initStandardObjects();
+                    finalResult = rhino.evaluateString(scriptable,process,"javascript",1,null).toString();
+                }catch (Exception e){
+                    finalResult="0";
+                }
+
+                mTextView2.setText(finalResult);
             }
         });
 
         btn_clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mEditText.setText("");
+                //input = "";
+                mTextView.setText("");
+                mTextView2.setText("");
             }
         });
     }
+
+    /*public void Solve() {
+        if (input.split("\\*").length == 2) {
+            String numbers[] = input.split("\\*");
+            try {
+                double mul = Double.parseDouble(numbers[0]) * Double.parseDouble(numbers[1]);
+                input = mul + "";
+            } catch (Exception e) {
+                //Display error
+            }
+        } else if (input.split("/").length == 2) {
+            String numbers[] = input.split("/");
+            try {
+                double div = Double.parseDouble(numbers[0]) / Double.parseDouble(numbers[1]);
+                input = div + "";
+            } catch (Exception e) {
+                //Display error
+            }
+        } else if (input.split("\\^").length == 2) {
+            String numbers[] = input.split("\\^");
+            try {
+                double pow = Math.pow(Double.parseDouble(numbers[0]), Double.parseDouble(numbers[1]));
+                input = pow + "";
+            } catch (Exception e) {
+                //Display error
+            }
+        } else if (input.split("\\+").length == 2) {
+            String numbers[] = input.split("\\+");
+            try {
+                double sum = Double.parseDouble(numbers[0]) + Double.parseDouble(numbers[1]);
+                input = sum + "";
+            } catch (Exception e) {
+                //Display error
+            }
+        } else if (input.split("\\-").length > 1) {
+            String numbers[] = input.split("\\-");
+            if (numbers[0] == "" && numbers.length == 2) {
+                numbers[0] = 0 + "";
+            }
+            try {
+                double sub = 0;
+                if (numbers.length == 2) {
+                    sub = Double.parseDouble(numbers[0]) - Double.parseDouble(numbers[1]);
+                } else if (numbers.length == 3) {
+                    sub = -Double.parseDouble(numbers[1]) - Double.parseDouble(numbers[2]);
+                }
+                input = sub + "";
+            } catch (Exception e) {
+                //Display error
+            }
+        }
+        String n[] = input.split("\\.");
+        if (n.length > 1) {
+            if (n[1].equals("0")) {
+                input = n[0];
+            }
+        }
+        mTextView.setText(input);
+
+    }*/
 
 }
 
